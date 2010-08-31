1bad23c9dac20e12c855e690f6856a45460c3825
Armin Häberling <armin.aha@gmail.com>
2010-08-31 23:34:29 +0200

    start vlc as independant process

--- src/tvtuner.py
+++ src/tvtuner.py
@@ -4,8 +4,10 @@ __author__="armin.aha@gmail.com"
 __date__ ="$Mar 15, 2010 8:45:58 PM$"
 
 import pylirc
-import subprocess
 import time
+
+import os
+import multiprocessing
 
 import logging
 
@@ -13,8 +15,63 @@ import ivtv_tuner
 
 logging.basicConfig(level=logging.DEBUG)
 
+
+def spawn_daemon(path_to_executable, args):
+    """Spawn a completely detached subprocess (i.e., a daemon).
+
+    E.g. for mark:
+    spawnDaemon("../bin/producenotify.py", "producenotify.py", "xx")
+    """
+    # fork the first time (to make a non-session-leader child process)
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise RuntimeError("1st fork failed: %s [%d]" % (e.strerror, e.errno))
+    if pid != 0:
+        # parent (calling) process is all done
+        return
+
+    # detach from controlling terminal (to make child a session-leader)
+    os.setsid()
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise RuntimeError("2nd fork failed: %s [%d]" % (e.strerror, e.errno))
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+    if pid != 0:
+        # child process is all done
+        os._exit(0)
+
+    # grandchild process now non-session-leader, detached from parent
+    # grandchild process must now close all open files
+    try:
+        maxfd = os.sysconf("SC_OPEN_MAX")
+    except (AttributeError, ValueError):
+        maxfd = 1024
+
+    os.closerange(0, maxfd)
+
+    # redirect stdin, stdout and stderr to /dev/null
+    os.open("/dev/null", os.O_RDONLY)    # standard input (0)
+    os.open("/dev/null", os.O_RDWR)      # standard output (1)
+    os.open("/dev/null", os.O_RDWR)      # standard error (2)
+
+
+    # and finally let's execute the executable for the daemon!
+    try:
+        os.execv(path_to_executable, args)
+    except Exception, e:
+        # oops, we're cut off from the world, let's just give up
+        os._exit(255)
+
+def spawn(path_to_executable, args):
+    p = multiprocessing.Process(target=spawn_daemon, args=(path_to_executable, args))
+    p.start()
+    p.join()
+
+
 def show_tv():
-    tv_process = subprocess.Popen(
+    spawn('/usr/bin/vlc',
         ['vlc',
          '--quiet',
          '--video-filter=deinterlace',
