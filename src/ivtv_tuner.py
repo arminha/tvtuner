4f4b9159c06a1bdec91959b673a823f19cb9866a
Armin Häberling <armin.aha@gmail.com>
2010-09-08 22:26:34 +0200

    code cleanup

--- src/ivtv_tuner.py
+++ src/ivtv_tuner.py
@@ -1,5 +1,5 @@
-__author__="armin.aha@gmail.com"
-__date__ ="$Mar 15, 2010 8:54:12 PM$"
+__author__ = 'armin.aha@gmail.com'
+__date__  = '$Mar 15, 2010 8:54:12 PM$'
 
 import re
 import subprocess
@@ -26,9 +26,15 @@ def check_output(*popenargs, **kwargs):
 
 class Tuner(object):
     """
-    Documentation
+    Tuner supporting ivtv-based tv tuner devices
     """
     def __init__(self, device):
+        """
+        Initialize Tuner.
+
+        :param device: The name of the tuner device (usually something like '/dev/video0')
+        :type device: string
+        """
         object.__init__(self)
         self.__channels = []
         self.__current_channel = 0
@@ -42,15 +48,27 @@ class Tuner(object):
         return self.__current_channel
 
     def set_channel(self, channel):
-        (_,frequency) = self.__channels[channel]
+        """
+        Set channel to the given channel.
+
+        :param channel: The channel to set
+        :type channel: int
+        """
+        (_, frequency) = self.__channels[channel]
         command = ['ivtv-tune', '-d', self.__device, '-f', frequency.__str__()]
         output = check_output(
             command,
             stderr=subprocess.STDOUT)
-        logging.debug('call "%s", output = "%s"' % (' '.join(command), output))
+        logging.debug('call "%s", output = "%s"', (' '.join(command), output))
         self.__current_channel = channel
 
     def next_channel(self):
+        """
+        Set channel to the next channel.
+
+        :return: The current channel
+        :rtype: int
+        """
         channel = self.__current_channel + 1
         if channel == len(self.__channels):
             channel = 0
@@ -58,6 +76,12 @@ class Tuner(object):
         return channel
 
     def prev_channel(self):
+        """
+        Set channel to the previous channel.
+
+        :return: The current channel
+        :rtype: int
+        """
         channel = self.__current_channel - 1
         if channel == -1:
             channel = len(self.__channels) - 1
@@ -65,13 +89,13 @@ class Tuner(object):
         return channel
 
     def init_channels(self, filename):
-        f = open(filename)
+        config_file = open(filename)
         self.__channels = []
-        line = f.readline()
+        line = config_file.readline()
         while line:
-            m = re.search('\{([^\}]*)\} ([0-9.]*) 0', line)
-            if m:
-                self.__channels.append((m.group(1), float(m.group(2))))
-            line = f.readline()
+            match = re.search('\{([^\}]*)\} ([0-9.]*) 0', line)
+            if match:
+                self.__channels.append((match.group(1), float(match.group(2))))
+            line = config_file.readline()
         if self.__channels:
             self.set_channel(0)
