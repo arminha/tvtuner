6f95ff289cc1168e9e611f4c9f4fdcf0c13753d6
Armin Häberling <armin.aha@gmail.com>
2010-09-22 21:22:48 +0200

    lazy initialize aosd

--- src/osd.py
+++ src/osd.py
@@ -16,24 +16,28 @@ class Osd(object):
     >>>osd.hide()
     """
     def __init__(self):
-        self._aosd = aosd.AosdText()
-        self._aosd.set_transparency(aosd.TRANSPARENCY_COMPOSITE)
+        self._aosd = None
         self._x_offset = 0
         self._y_offset = 0
         self._position = 0
-        self._setup()
 
     def show(self, text):
+        if not self._aosd:
+            self._setup()
         self._set_text(text)
         self._aosd.show()
         self._aosd.loop_once()
 
     def show_for(self, text, milliseconds):
+        if not self._aosd:
+            self._setup()
         self._set_text(text)
         self._aosd.show()
         self._aosd.loop_for(milliseconds)
 
     def hide(self):
+        if not self._aosd:
+            self._setup()
         self._aosd.hide()
         self._aosd.loop_once()
 
@@ -45,6 +49,9 @@ class Osd(object):
         self._aosd.render()
 
     def _setup(self):
+        self._aosd = aosd.AosdText()
+        self._aosd.set_transparency(aosd.TRANSPARENCY_COMPOSITE)
+
         self._aosd.geom_x_offset = 20
         self._aosd.geom_y_offset = 10
 
