bdc2e5fe30ec163ee63d2c4feb688d1bb223d481
Armin Häberling <armin.aha@gmail.com>
2010-10-05 22:38:16 +0200

    remove aosd, add dependency to aosd

--- setup.py
+++ setup.py
@@ -2,55 +2,16 @@
 # coding=utf8
 
 from distutils.core import setup
-from distutils.extension import Extension
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    from Pyrex.Distutils import build_ext
-
-import commands
-
-def pkgconfig_include_dirs(*packages):
-    flag_map = {'-I': 'include_dirs'}
-    kw = {}
-    for token in commands.getoutput("pkg-config --cflags %s" % ' '.join(packages)).split():
-        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-    return kw['include_dirs']
-
-#def pkgconfig_libraries(*packages):
-#    flag_map = {'-l': 'libraries'}
-#    kw = {}
-#    for token in commands.getoutput("pkg-config --libs %s" % ' '.join(packages)).split():
-#        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-#    return kw['libraries']
-#
-#def pkgconfig_library_dirs(*packages):
-#    flag_map = {'-L': 'library_dirs'}
-#    kw = {}
-#    for token in commands.getoutput("pkg-config --libs %s" % ' '.join(packages)).split():
-#        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-#    return kw['library_dirs']
-
 
 setup (
-    cmdclass = {'build_ext' : build_ext},
-
     name = 'tvtuner',
     version = '0.2.4',
     package_dir = {'': 'src'},
     packages = [''],
-    ext_modules = [
-        Extension(
-            'aosd',
-            ['src/aosd.pyx'],
-            include_dirs= pkgconfig_include_dirs('pangocairo') + ['/usr/include/libaosd'],
-            libraries = ['aosd', 'aosd-text']
-        )
-    ],
     scripts = ['src/tvtuner'],
 
     # Declare your packages' dependencies here, for eg:
-    requires=['pylirc', 'yaml'],
+    requires=['pylirc', 'yaml', 'aosd'],
 
     # Fill in these to make your Egg ready for upload to
     # PyPI
