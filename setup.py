cea9bb5b81050a9eefbe0bc243cf9d3285f89389
Armin Häberling <armin.aha@gmail.com>
2010-12-24 16:57:44 +0100

    set version = 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup (
     name = 'tvtuner',
-    version = '0.3.0',
+    version = '0.3.1',
     package_dir = {'': 'src'},
     packages = [''],
     scripts = ['src/tvtuner'],
