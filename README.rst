7e8af7f8205aaa5f98236782ed5e87379f3c5cd6
Armin Häberling <armin.aha@gmail.com>
2012-10-03 16:17:33 +0200

    Update description and add README and LICENSE.

---
+++ README.rst
@@ -0,0 +1,12 @@
+tvtuner
+=======
+
+A script to control `IVTV <http://ivtvdriver.org/>`__ based tv cards with an IR remote.
+
+Dependencies
+------------
+
+- `pyLirc <http://www.lirc.org>`__
+- `PyYAML <http://pyyaml.org/>`__
+- `python-aosd <http://code.google.com/p/python-aosd/>`__
+
--- setup.py
+++ setup.py
@@ -10,15 +10,17 @@ setup (
     packages = [''],
     scripts = ['src/tvtuner'],
 
-    # Declare your packages' dependencies here, for eg:
     requires=['pylirc', 'yaml', 'aosd'],
 
-    # Fill in these to make your Egg ready for upload to
-    # PyPI
     author = 'Armin Häberling',
     author_email = 'armin.aha@gmail.com',
 
-    description = 'TV tuner for ivtv cards.',
-    license = '',
-    long_description= 'Long description of the package',
+    description = 'Script to control IVTV based tv cards with an IR remote.',
+    license = 'GPL',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: POSIX :: Linux'
+        'Programming Language :: Python'
+    ]
 )
